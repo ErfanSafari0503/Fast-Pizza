1faf36fa7e77d9625493cc393c5e5a8ad9f0dc62
Erfan Safari <erfansafari@Erfans-MacBook-Pro.local>
2025-05-14 21:04:37 +0330

    Setting up React-Router with data loading.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,48 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder, {
+  action as creatOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoadder } from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: creatOrderAction,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoadder,
+      },
+    ],
+  },
+]);
+
 export default function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
